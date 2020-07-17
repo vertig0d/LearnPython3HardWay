1cc92c8673a9107001033ca30d8cd15a76b04064
vertig0d <geni.zero1@outlook.com>
2020-07-17 11:15:37 +0000

    ex5 updated2

--- Ex5_MoreVariablesAndPrinting.py
+++ Ex5_MoreVariablesAndPrinting.py
@@ -1,1 +1,15 @@
-test file
+my_name = 'zed Shaw' #author of the book, not me obviously
+my_age = 35
+my_height = 74 # inches
+my_weight = 180 #lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print(f'Lets talk about {my_name}')
+print(f'he is {my_age} years old')
+print(f'hes about {my_height} inches tall and weighs about {my_weight} lbs')
+print(f'his eyes are {my_eyes} and teeth are {my_teeth} depending on the coffee')
+
+total = my_age + my_height + my_weight
+print(f'if i add {my_age}, {my_height}, {my_weight} i get {total}')
