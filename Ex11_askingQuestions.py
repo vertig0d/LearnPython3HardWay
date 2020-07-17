b488077bffe52f11c30bff3305e88924d90ec1cb
vertig0d <geni.zero1@outlook.com>
2020-07-17 19:50:19 +0000

    ex11

---
+++ Ex11_askingQuestions.py
@@ -0,0 +1,8 @@
+print('how old are you?',end='')
+age = input()
+print('how tall are you?')
+height = input()
+print('how much do you weigh')
+weight = input()
+print(f'so you are {age} years old, {height} tall and weigh {weight}')
+
