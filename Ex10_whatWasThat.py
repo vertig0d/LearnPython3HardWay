49652ebbf8cf3c93133f131a27e6d112f1d8df17
vertig0d <geni.zero1@outlook.com>
2020-07-17 18:29:28 +0000

    ex10

---
+++ Ex10_whatWasThat.py
@@ -0,0 +1,16 @@
+tabby_cat = '\tIm a tabbed in'
+persian_cat = 'im split\non a line'
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+i'll do a list:
+\t*Cat food
+\t*Fishes
+\t*Catnip\n\t*Grass
+	"""
+	
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+	
